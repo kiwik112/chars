2af50d63dd9e26029b7af9533b0fae0ab0eb096a
kiwik112 <pakiwi06@gmail.com>
2025-01-27 16:57:24 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╒═════════╕\n│         │\n│   box   │\n│         │\n╘═════════╛");
+			Console.ReadLine();
+        }
+    }
+}
